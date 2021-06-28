d6ddb84640605d6825020c79bea9edb2812aed3e
Harriette Berndes <harrietteberndes@BRIGHT-OS-01.local>
2021-06-28 14:17:47 +0100

    initial commit

---
+++ networking-with-film-api/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  networking-with-film-api
+//
+//  Created by Harriette Berndes on 28/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+    }
+
+
+}
+
